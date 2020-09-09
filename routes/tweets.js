510d83e2e79a620df757e3fd03a4721df2414a25
Adam Hussain <adamh93@verizon.net>
2020-09-08 20:54:23 -0400

    done: Make API Get request to twitter API with query searching for tweets about COVID19, Add variables to env needed to communicate with Twitter API

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const Twit = require('twit')
 
+var T = new Twit({
+  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+  access_token:         process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
 
 router.get('/', async(req, res) => {
+    T.get('statuses/user_timeline', {q:'COVID-19', screen_name: 'CNN', in_reply_to_status_id: null, exclude_replies:true, include_rts:false, count: 10 }, function(err, data, response) {
+    console.log(data)
+  })
+
   res.send('Tweet Success')
 })
 
